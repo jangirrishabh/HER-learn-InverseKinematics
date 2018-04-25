0da97e445ec0f4d2f4c59980bb86ac0985cd164b
jangirrishabh <jangirrishabh@gmail.com>
2018-04-25 15:43:10 +0200

    No object final commit, added multiple goals environment

--- gym_gazebo/envs/barret_wam/gazebo_wam_multiple_goals.py
+++ gym_gazebo/envs/barret_wam/gazebo_wam_multiple_goals.py
@@ -19,25 +19,19 @@ from gym.utils import seeding
 from numpy import linalg as LA
 from iri_common_drivers_msgs.srv import QueryInverseKinematics
 from iri_common_drivers_msgs.srv import QueryForwardKinematics
-from gazebo_msgs.msg import ModelStates
+
 
 def goal_distance(goal_a, goal_b):
     assert goal_a.shape == goal_b.shape
     return np.linalg.norm(goal_a - goal_b, axis=-1)
 
-def roundOffList(lyst):
-    newLyst = []
-    for i in lyst:
-        newLyst.append(round(i, 4))
-
-    return newLyst
 
 
-class GazeboWAMemptyEnvv1(gazebo_env.GazeboEnv):
+class GazeboWAMemptyEnvv2(gazebo_env.GazeboEnv):
 
     def __init__(self):
         # Launch the simulation with the given launchfile name
-        gazebo_env.GazeboEnv.__init__(self, "iri_wam_HER.launch")
+        gazebo_env.GazeboEnv.__init__(self, "iri_wam.launch")
         self.publishers = ['pub1', 'pub2', 'pub4', 'pub3', 'pub5', 'pub6', 'pub7'] #publishers for the motor commands
 
         self.publishers[0] = rospy.Publisher('/iri_wam/joint1_position_controller/command', Float64, queue_size=5)
@@ -47,7 +41,7 @@ class GazeboWAMemptyEnvv1(gazebo_env.GazeboEnv):
         self.publishers[4] = rospy.Publisher('/iri_wam/joint5_position_controller/command', Float64, queue_size=5)
         self.publishers[5] = rospy.Publisher('/iri_wam/joint6_position_controller/command', Float64, queue_size=5)
         self.publishers[6] = rospy.Publisher('/iri_wam/joint7_position_controller/command', Float64, queue_size=5) # discretely publishing motor actions for now
-        self.pubMarker = rospy.Publisher('/goalPose', Marker, queue_size=5)
+        self.pubMarker = rospy.Publisher('/goalPose', Marker, queue_size=5)  ##we might need multiple markers here
 
         self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
         self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
@@ -56,12 +50,13 @@ class GazeboWAMemptyEnvv1(gazebo_env.GazeboEnv):
         #self.minDisplacement = 0.02 #minimum discrete distance by a single action 
         #self.minDisplacementTolerance = 0.01
         #self.minDisplacementPose = self.minDisplacement + self.minDisplacementTolerance  # minimum distance to check for the goal reaching state
-        self.distanceThreshold = 0.09
+        self.distanceThreshold = 0.03
         self.baseFrame = 'iri_wam_link_base' 
         self.homingTime = 0.6 # time given for homing
         self.lenGoal = 3 # goal position list length
         self._max_episode_steps = 100
         self.reward_type = 'sparse'
+        self.num_goals = 3
               
         
 
@@ -80,11 +75,11 @@ class GazeboWAMemptyEnvv1(gazebo_env.GazeboEnv):
         self.lowConcObs = np.array([-2.6, -1.42, -0.88]) #1, 2, 4
         self.highConcObs = np.array([2.6, 1.42, 3.08])
 
-        self.samplelow = np.array([-2.4, -1.4, -2.03, -0.68, -4.04, -1.05, -2.08])
-        self.samplehigh = np.array([2.4, 1.4, 2.03, 2.78, 1.0, 1.05, 2.08])
+        #self.samplelow = np.array([-2.4, -1.4, -2.03, -0.68, -4.04, -1.05, -2.08])
+        #self.samplehigh = np.array([2.4, 1.4, 2.03, 2.78, 1.0, 1.05, 2.08])
 
-        #self.samplelow = np.array([-1.5, -0.8, -2.03, -0.48, -4.04, -1.05, -2.08])
-        #self.samplehigh = np.array([1.5, 0.8, 2.03, 2.48, 1.0, 1.05, 2.08])
+        self.samplelow = np.array([-1.5, -0.8, -2.03, -0.48, -4.04, -1.05, -2.08])
+        self.samplehigh = np.array([1.5, 0.8, 2.03, 2.48, 1.0, 1.05, 2.08])
         #self.high = np.array([5.2, 2.8, 5.4, 3.96, 6.96, 3.1, 5.96])        
         self.lowAction = [-1, -1, -1]
         self.highAction = [1, 1, 1]
@@ -92,8 +87,8 @@ class GazeboWAMemptyEnvv1(gazebo_env.GazeboEnv):
 
         self.lastObservation = None
         self.lastObservationJS = None
-        self.goal = None
-        self.goalJS = None
+        self.goal = [None]*self.num_goals
+        self.goalJS = [None]*self.num_goals
 
         self.action_space = spaces.Box(-1., 1., shape=(self.n_actions,), dtype='float32')
 
@@ -129,26 +124,28 @@ class GazeboWAMemptyEnvv1(gazebo_env.GazeboEnv):
 
     def _sample_goal(self): #sample from reachable positions
         frame_ID = self.baseFrame
-        tempPoseFK = None
+        tempPoseFK = [None]*self.num_goals
 
-        while tempPoseFK==None:
-            tempPosition = []
-            for joint in range(4):
-                tempPosition.append(random.uniform(self.samplelow[joint], self.samplehigh[joint]))
+        while np.array(tempPoseFK).any()==None:
+            for x in range(self.num_goals):
+                tempPosition = []
+                for joint in range(4):
+                    tempPosition.append(random.uniform(self.samplelow[joint], self.samplehigh[joint]))
 
-            tempPosition.append(0)
-            tempPosition.append(0)
-            tempPosition.append(0)
-            tempPosition[2] = 0
-            tempJointState = JointState()
-            tempJointState.header.frame_id = self.baseFrame
-            tempJointState.position = tempPosition
+                tempPosition.append(0)
+                tempPosition.append(0)
+                tempPosition.append(0)
+                tempPosition[2] = 0
+                tempJointState = JointState()
+                tempJointState.header.frame_id = self.baseFrame
+                tempJointState.position = tempPosition
 
-            self.goalJS = tempPosition
-            tempPoseFK = self.getForwardKinematics(tempJointState)
-            if tempPoseFK!=None:
-                tempTemp = [tempPoseFK.pose.pose.position.x, tempPoseFK.pose.pose.position.y, tempPoseFK.pose.pose.position.z]
-                return np.array(tempTemp)
+                
+                tempTemp = self.getForwardKinematics(tempJointState)
+                tempPoseFK[x] = [tempTemp.pose.pose.position.x, tempTemp.pose.pose.position.y, tempTemp.pose.pose.position.z]
+        
+        self.goalJS = tempPoseFK
+        return np.array(tempPoseFK)
                 
 
     def seed(self, seed=None):
@@ -216,15 +213,11 @@ class GazeboWAMemptyEnvv1(gazebo_env.GazeboEnv):
 
     def _get_obs(self):
         data = None
-        objectState = None
-        gripperPos = self.lastObservation   
-
+        gripperPos = self.lastObservation
+    
         while data is None:
             try:
                 data = rospy.wait_for_message('/joint_states', JointState, timeout=1)
-                # temp = rospy.wait_for_message('/gazebo/model_states', ModelStates, timeout=1)
-                # objectState = [temp.pose[3].position.x, temp.pose[3].position.y, temp.pose[3].position.z]
-                # print("Object state ios ", objectState)
                 gripperPos = self.getGripperPosition(data.position) #cartesian coordinates of the gripper
                 dataConc = np.array([data.position[0], data.position[1], data.position[3]]) # joint space coordinates of the robotic arm 1, 2, 4
                 if ((np.array(dataConc)<=self.highConcObs).all()) and ((np.array(dataConc)>=self.lowConcObs).all()): #check if they lie inside allowed envelope
@@ -326,6 +319,29 @@ class GazeboWAMemptyEnvv1(gazebo_env.GazeboEnv):
             
 
         self.pubMarker.publish(markerObj)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
 
 
 
