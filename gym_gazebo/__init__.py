8ccffeeea645f5c698890085e3ba53973d2936eb
jangirrishabh <jangirrishabh@gmail.com>
2018-06-07 16:54:58 +0200

    getting the repo world ready

--- gym_gazebo/__init__.py
+++ gym_gazebo/__init__.py
@@ -6,98 +6,6 @@ logger = logging.getLogger(__name__)
 # Gazebo
 # ----------------------------------------
 
-# Turtlebot envs
-register(
-    id='GazeboMazeTurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboMazeTurtlebotLidarEnv',
-    # More arguments here
-)
-register(
-    id='GazeboCircuitTurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboCircuitTurtlebotLidarEnv',
-    # More arguments here
-)
-register(
-    id='GazeboCircuit2TurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboCircuit2TurtlebotLidarEnv',
-    # More arguments here
-)
-register(
-    id='GazeboCircuit2TurtlebotLidarNn-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboCircuit2TurtlebotLidarNnEnv',
-    # More arguments here
-)
-register(
-    id='GazeboCircuit2cTurtlebotCameraNnEnv-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboCircuit2cTurtlebotCameraNnEnv',
-    # More arguments here
-)
-register(
-    id='GazeboRoundTurtlebotLidar-v0',
-    entry_point='gym_gazebo.envs.turtlebot:GazeboRoundTurtlebotLidarEnv',
-    # More arguments here
-)
-
-# Erle-Copter envs
-register(
-    id='GazeboErleCopterHover-v0',
-    entry_point='gym_gazebo.envs.erlecopter:GazeboErleCopterHoverEnv',
-)
-
-#Erle-Rover envs
-register(
-    id='GazeboMazeErleRoverLidar-v0',
-    entry_point='gym_gazebo.envs.erlerover:GazeboMazeErleRoverLidarEnv',
-)
-
-# Modular SCARA
-register(
-    id='GazeboModularScara3DOF-v0',
-    entry_point='gym_gazebo.envs.modular_scara:GazeboModularScara3DOFEnv',
-)
-register(
-    id='GazeboModularScara3DOF-v1',
-    entry_point='gym_gazebo.envs.modular_scara:GazeboModularScara3DOFv1Env',
-)
-register(
-    id='GazeboModularScara3DOF-v2',
-    entry_point='gym_gazebo.envs.modular_scara:GazeboModularScara3DOFv2Env',
-)
-register(
-    id='GazeboModularScara3DOF-v3',
-    entry_point='gym_gazebo.envs.modular_scara:GazeboModularScara3DOFv3Env',
-)
-register(
-    id='GazeboModularScara4DOF-v3',
-    entry_point='gym_gazebo.envs.modular_scara:GazeboModularScara4DOFv3Env',
-)
-register(
-    id='GazeboModularArticulatedArm4DOF-v1',
-    entry_point='gym_gazebo.envs.articulated_arm:GazeboModularArticulatedArm4DOFv1Env',
-)
-
-register(
-    id='GazeboModularScaraObstacles3DOF-v0',
-    entry_point='gym_gazebo.envs.modular_scara:GazeboModularScara3DOFObstaclesv0Env',
-)
-register(
-    id='GazeboModularScaraStaticObstacle3DOF-v0',
-    entry_point='gym_gazebo.envs.modular_scara:GazeboModularScara3DOFStaticObstaclev0Env',
-)
-register(
-    id='GazeboModularScaraStaticObstacle3DOF-v1',
-    entry_point='gym_gazebo.envs.modular_scara:GazeboModularScara3DOFStaticObstaclev1Env',
-)
-register(
-    id='GazeboModularScaraArm4And3DOF-v1',
-    entry_point='gym_gazebo.envs.modular_scara:GazeboModularScara4And3DOFv1Env',
-)
-
-# cart pole
-register(
-    id='GazeboCartPole-v0',
-    entry_point='gym_gazebo.envs.gazebo_cartpole:GazeboCartPolev0Env',
-)
 
 # barret wam
 
@@ -114,10 +22,4 @@ register(
 register(
     id='GazeboWAMemptyEnv-v2',
     entry_point='gym_gazebo.envs.barret_wam:GazeboWAMemptyEnvv2',
-)
-
-# ARIACPickv0Env
-register(
-    id='ARIACPick-v0',
-    entry_point='gym_gazebo.envs.ARIAC:ARIACPickv0Env',
 )
